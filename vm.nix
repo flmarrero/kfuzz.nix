31ce8946eef9690e4d173f653c8f35ad0e901190
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2025-06-07 11:49:35 +0200

    Switch to testing kernel

--- vm.nix
+++ vm.nix
@@ -1,62 +1,56 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackagesFor (import ./kernel.nix {
-        inherit (pkgs) lib stdenv fetchgit buildLinux;
-    });
+    kernelPackages = pkgs.linuxPackagesFor
+      (pkgs.linuxPackages_testing.kernel.override {
+        structuredExtraConfig = with lib.kernel; {
+          # Coverage & Debugging
+          KCOV = yes;
+          KCOV_INSTRUMENT_ALL = yes;
+          DEBUG_INFO = yes;
+          DEBUG_KERNEL = yes;
+          DEBUG_LIST = yes;
+          DEBUG_OBJECTS = yes;
+          DEBUG_SLAB = yes;
+          DEBUG_VM = yes;
+          STACKTRACE = yes;
+          GDB_SCRIPTS = yes;
+
+          # Address Sanitizer
+          KASAN = yes;
+          KASAN_INLINE = yes;
+          KASAN_STACK = yes;
+          KCSAN = yes;
+          KCSAN_REPORT_ONCE = yes;
+
+          # Lockup Detection
+          LOCKUP_DETECTOR = yes;
+          HARDLOCKUP_DETECTOR = yes;
+          SOFTLOCKUP_DETECTOR = yes;
+          DETECT_HUNG_TASK = yes;
+
+          # Crash Dumps
+          CRASH_DUMP = yes;
+          PROC_VMCORE = yes;
+          VMCOREINFO = yes;
+
+          # Panic handling
+          PANIC_ON_OOPS = yes;
+          BUG = yes;
+          SCHED_STACK_END_CHECK = yes;
+
+          # Other useful options
+          MAGIC_SYSRQ = yes;
+          DEBUG_FS = yes;
+          FAULT_INJECTION = yes;
+          FAULT_INJECTION_DEBUG_FS = yes;
+        };
+        ignoreConfigErrors = true;
+      });
 
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-
-    kernelPatches = [{
-      name = "fuzzing-kernel-config";
-      patch = null;
-      extraConfig = ''
-        # Coverage & Debugging
-        KCOV y
-        KCOV_INSTRUMENT_ALL y
-        DEBUG_INFO y
-        DEBUG_KERNEL y
-        DEBUG_LIST y
-        DEBUG_OBJECTS y
-        DEBUG_SLAB y
-        DEBUG_VM y
-        STACKTRACE y
-        GDB_SCRIPTS y
-
-        # Address Sanitizer
-        KASAN y
-        KASAN_INLINE y
-        KASAN_STACK y
-
-        # Race Detector
-        KCSAN y
-        KCSAN_REPORT_ONCE y
-
-        # Lockup Detection
-        LOCKUP_DETECTOR y
-        HARDLOCKUP_DETECTOR y
-        SOFTLOCKUP_DETECTOR y
-        DETECT_HUNG_TASK y
-
-        # Crash Dumps
-        CRASH_DUMP y
-        PROC_VMCORE y
-        VMCOREINFO y
-
-        # Panic handling
-        PANIC_ON_OOPS y
-        BUG y
-        SCHED_STACK_END_CHECK y
-
-        # Other useful options
-        MAGIC_SYSRQ y
-        DEBUG_FS y
-        FAULT_INJECTION y
-        FAULT_INJECTION_DEBUG_FS y
-      '';
-    }];
   };
 
   users.extraUsers.root.initialHashedPassword = "";
